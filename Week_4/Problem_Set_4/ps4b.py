429911f20283deb61aefa87a0b1c8747b1134fb9
henry199101 <331243543@qq.com>
2017-05-23 16:39:36 +0800

    commit ^C

--- Week_4/Problem_Set_4/ps4b.py
+++ Week_4/Problem_Set_4/ps4b.py
@@ -12,8 +12,7 @@ def compChooseWord(hand, wordList, n):
     Given a hand and a wordList, find the word that gives 
     the maximum value score, and return it.
 
-    This word should be calculated by considering all the words
-    in the wordList.
+    This word should be calculated by considering all the words in the wordList.
 
     If no words in the wordList can be made from the hand, return None.
 
@@ -25,23 +24,22 @@ def compChooseWord(hand, wordList, n):
     """
     # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Create a new variable to store the maximum score seen so far (initially 0)
-
-    # Create a new variable to store the best word seen so far (initially None)  
-
+    # Create a new variable to store the best word seen so far (initially None)
+    score = 0
+    bestWord = None            
     # For each word in the wordList
-
-        # If you can construct the word from your hand
+    for word in wordList:
+        # If you can construct(¹¹³É) the word from your hand
         # (hint: you can use isValidWord, or - since you don't really need to test if the word is in the wordList - you can make a similar function that omits that test)
-
-            # Find out how much making that word is worth
-
+        if isValidWord(word, hand, wordList) == True:
+            # Find out how much making that word is worth                
             # If the score for that word is higher than your best score
-
+            if getWordScore(word, n) > score:                
                 # Update your best score, and best word accordingly
-
-
+                score = getWordScore(word, n)
+                bestWord = word
     # return the best word you found.
-
+    return bestWord
 
 #
 # Problem #7: Computer plays a hand
